bce5edf18ead129b343bd9cf00660f1a319dd948
Tomás Leggire <tomasleggire@hotmail.com>
2022-09-16 21:28:24 -0300

    3er commit de esta carpeta

---
+++ code.js
@@ -0,0 +1,17 @@
+const h1 = document.querySelector("h1");
+
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+
+const btn = document.querySelector("#btnCalcular");
+
+const p = document.querySelector("#resultado");
+
+const form = document.querySelector("#formulario")
+
+btn.addEventListener("click",()=> {
+   let result = Number(input1.value) +  Number(input2.value);
+   console.log(result);
+   p.textContent = "El resultado es " + result;
+})
+
